be4517911bfdd6a0f1e81bbb337e0d537881d64d
Marius Gžegoževskis <m.gzegozevskis@eif.viko.lt>
2021-09-05 23:42:18 +0300

    Generated test html files

--- src/test/java/PassinkTestas.java
+++ src/test/java/PassinkTestas.java
@@ -1,17 +1,16 @@
 import io.cucumber.junit.Cucumber;
-
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = "src/test/resources/features",
-    glue = {"objectfortesting"},
-    tags = "@is_it_friday_yet",
+    glue = {"passed"},
+    tags = "@reset",
     plugin = {
         "pretty",
         "json:build/cucumber-report/cucumber.json",
         "html:build/cucumber-report/cucumber.html",
         "junit:build/cucumber-report/cucumber.xml"}
 )
-public class RunTests { }
+public class PassinkTestas { }
