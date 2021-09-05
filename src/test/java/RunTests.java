6c63afe7b266ef76c3fd075c53c035336b48891f
Marius Gžegoževskis <m.gzegozevskis@eif.viko.lt>
2021-09-05 22:29:45 +0300

    Fixed dependencies

--- src/test/java/RunTests.java
+++ src/test/java/RunTests.java
@@ -1,6 +1,8 @@
 import io.cucumber.junit.Cucumber;
-import org.junit.runner.RunWith;
+
 import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = "src/test/resources/features",
